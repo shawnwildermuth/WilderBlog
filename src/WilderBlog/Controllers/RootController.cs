9a3cedb06697b6ee7abb58a92faf339216adc75d
Shawn Wildermuth <shawn@wildermuth.com>
2016-01-03 17:16:23 -0500

    Integration of Template and Images

---
+++ src/WilderBlog/Controllers/RootController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace WilderBlog.Controllers
+{
+  [Route("")]
+  public class RootController : Controller
+  {
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
