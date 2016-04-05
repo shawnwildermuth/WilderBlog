6af5d1327cfac5a5c46267a7ffe371db1d9eb9de
Shawn Wildermuth <shawn@wildermuth.com>
2016-04-05 00:04:42 -0400

    Adding Sidebar

---
+++ src/WilderBlog/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Site-wide JavaScript
+
+// Search Form
+(function ($) {
+  $("#searchForm").submit(function (e) {
+    window.location = "/search/" + encodeURI($("#search").val());
+    e.preventDefault();
+    return false;
+  });
+})(jQuery);
