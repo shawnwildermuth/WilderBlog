7bbf553fdf41d5ab7e00942ded1472b294af0f56
Shawn Wildermuth <shawn@wildermuth.com>
2016-05-10 15:53:24 +0200

    Reorganizing Controllers and adding TourMap to the project

---
+++ src/WilderBlog/Controllers/Api/EpisodeController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
+using WilderBlog.Services.DataProviders;
+
+namespace WilderBlog.Controllers.Api
+{
+  [Route("/api/episodes")]
+  public class EpisodeController : Controller
+  {
+    private PodcastEpisodesProvider _provider;
+    const int PAGE_SIZE = 10;
+    private ILogger<EpisodeController> _logger;
+
+    public EpisodeController(PodcastEpisodesProvider podcastProvider, ILogger<EpisodeController> logger)
+    {
+      _provider = podcastProvider;
+      _logger = logger;
+    }
+
+    IEnumerable<PodcastEpisode> LiveEpisodes => _provider.Get()
+                                                  .Where(e => e.Status == PodcastEpisodeStatus.Live && e.PublishedDate <= DateTime.UtcNow)
+                                                  .OrderBy(e => e.EpisodeNumber);
+
+    [HttpGet("{page:int}")]
+    public IActionResult Get(int page = 1, int pageSize = PAGE_SIZE)
+    {
+      try
+      {
+        var data = LiveEpisodes;
+
+        var results = new
+        {
+          Success = true,
+          ResultCount = data.Count(),
+          Page = page,
+          Results = data.Skip(pageSize * (page - 1)).Take(pageSize)
+        };
+
+        return Ok(results);
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError("Failed to get episodes from the API", ex);
+        return HttpBadRequest();
+      }
+
+    }
+
+    [HttpGet("{number:int}")]
+    public IActionResult Get(int number)
+    {
+      try
+      {
+        var data = LiveEpisodes
+                  .FirstOrDefault(e => e.EpisodeNumber == number);
+
+        return Ok(data);
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError("Failed to get episode from the API", ex);
+
+        return HttpBadRequest();
+      }
+    }
+
+  }
+}
---
+++ src/WilderBlog/wwwroot/js/tourmap.js
@@ -0,0 +1,33 @@
+﻿// tourmap.js
+(function ($) {
+
+  var map;
+  var currentLocNumber = 55;
+
+  $(document).ready(function () {
+
+    $('head').append("<style> #map img{max-width: inherit;} .gm-style-iw{ overflow: hidden !important;}</style>")
+
+    var url = "http://wildermuth.com/js/tourdates.js";
+    //var url = "/js/tourdates.js";
+
+
+    $.get(url, function (data) {
+
+      var lines = eval(data);
+
+      $.each(lines, function (i, stop) {
+        stop.info = stop.info.replace(" - ", "<br/>");
+      });
+
+      var map = travelMap.createMap({
+        stops: lines,
+        currentStop: currentLocNumber,
+        selector: "#map"
+      });
+
+    });
+
+  });
+
+})(jQuery);
