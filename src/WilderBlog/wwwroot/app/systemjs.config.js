2ce2dce7f8c14b6151ca13f20b214c858db13ef8
Shawn Wildermuth <shawn@wildermuth.com>
2016-06-18 18:00:20 -0500

    Update for Angular2 RC2

--- src/WilderBlog/wwwroot/app/systemjs.config.js
+++ src/WilderBlog/wwwroot/app/systemjs.config.js
@@ -3,34 +3,31 @@
   // map tells the System loader where to look for things
   var map = {
     'app': 'app', // 'dist',
-    'rxjs': 'angular2',
-    'angular2-in-memory-web-api': 'angular2/angular2-in-memory-web-api',
-    '@angular': 'angular2'
+    'rxjs': 'lib/rxjs',
+    '@angular': 'lib/angular2'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app': { main: 'main.js', defaultExtension: 'js' },
-    'rxjs': { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    'rxjs': { defaultExtension: 'js' }
   };
 
   var packageNames = [
-      '@angular/common',
-      '@angular/compiler',
-      '@angular/core',
-      '@angular/http',
-      '@angular/platform-browser',
-      '@angular/platform-browser-dynamic',
-      '@angular/router',
-      '@angular/router-deprecated',
-      '@angular/testing',
-      '@angular/upgrade'
+      'common',
+      'compiler',
+      'core',
+      'http',
+      'platform-browser',
+      'platform-browser-dynamic',
+      'router',
+      'router-deprecated',
+      'testing',
+      'upgrade'
   ];
 
-  // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
   packageNames.forEach(function (pkgName) {
-    packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
   });
 
   var config = {
