585dd63e52c3bc6bf2f865e6e15b8c520377cd41
Shawn Wildermuth <shawn@wildermuth.com>
2017-11-17 14:09:46 -0500

    Update to remove bower

--- src/WilderBlog/gulpfile.js
+++ src/WilderBlog/gulpfile.js
@@ -1,4 +1,4 @@
-/// <binding Clean='default' />
+/// <binding Clean='clean, minify, scripts' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -7,23 +7,85 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var del = require("del");
+var merge = require('merge-stream'); 
 
 gulp.task("minify", function () {
-  gulp.src(["wwwroot/js/*.js", '!wwwroot/js/tour*.js', '!wwwroot/js/contact.js'])
-    .pipe(uglify())
-    .pipe(concat("wilderblog.min.js"))
-    .pipe(gulp.dest("wwwroot/lib/site"));
-  gulp.src(["wwwroot/js/contact.js"])
-    .pipe(uglify())
-    .pipe(concat("contact.min.js"))
-    .pipe(gulp.dest("wwwroot/lib/site"));
+
+  var streams = [
+    gulp.src(["wwwroot/js/*.js", '!wwwroot/js/tour*.js', '!wwwroot/js/contact.js'])
+      .pipe(uglify())
+      .pipe(concat("wilderblog.min.js"))
+      .pipe(gulp.dest("wwwroot/lib/site")),
+    gulp.src(["wwwroot/js/contact.js"])
+      .pipe(uglify())
+      .pipe(concat("contact.min.js"))
+      .pipe(gulp.dest("wwwroot/lib/site"))
+  ];
+
+  return merge(streams);
+});
+
+// Copy Dependencies
+var deps = {
+  "jquery": {
+    "dist/*": ""
+  },
+  "bootstrap": {
+    "dist/**/*": ""
+  },
+  "font-awesome": {
+    "css/*": "css",
+    "fonts/*": "fonts"
+  },
+  "cookieconsent": {
+    "build/*": ""
+  },
+  "highlightjs": {
+    "*.js": "",
+    "styles/*": "styles"
+  },
+  "lodash": {
+    "lodash*.*": ""
+  },
+  "popper.js": {
+    "dist/*": ""
+  },
+  "respond.js": {
+    "dest/*": ""
+  },
+  "tether": {
+    "dist/**/*": ""
+  },
+  "vue": {
+    "dist/*": ""
+  },
+  "vee-validate": {
+    "dist/*": ""
+  },
+  "vue-resource": {
+    "dist/*": ""
+  },
+
+};
+
+gulp.task("clean", function () {
+  return del("wwwroot/lib/**/*");
 });
 
 gulp.task("scripts", function () {
-  gulp.src("node_modules/bootstrap/dist/js/*")
-    .pipe(gulp.dest("wwwroot/lib/bootstrap/js"));
-  gulp.src("node_modules/bootstrap/dist/css/*")
-    .pipe(gulp.dest("wwwroot/lib/bootstrap/css"));
+
+  var streams = [];
+
+  for (var prop in deps) {
+    console.log("Prepping Scripts for: " + prop);
+    for (var itemProp in deps[prop]) {
+      streams.push(gulp.src("node_modules/" + prop + "/" + itemProp)
+        .pipe(gulp.dest("wwwroot/lib/" + prop + "/" + deps[prop][itemProp])));
+    }
+  }
+
+  return merge(streams);
+
 });
 
-gulp.task('default', ["minify", "scripts"]);
