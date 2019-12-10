5f116368cac1f479ca42066a1cf73caaba8dedd9
Shawn Wildermuth <shawn@wildermuth.com>
2019-12-09 19:39:27 -0500

    Adding new Ads

--- src/WilderBlog/Services/AdService.cs
+++ src/WilderBlog/Services/AdService.cs
@@ -32,7 +32,9 @@ namespace WilderBlog.Services
           @"<a href=""//pluralsight.pxf.io/c/1191850/595252/7490"" id=""595252""><img src=""//a.impactradius-go.com/display-ad/7490-595252"" border=""0"" alt="""" width=""468"" height=""60""/></a><img height=""0"" width=""0"" src=""//pluralsight.pxf.io/i/1191850/595252/7490"" style=""position:absolute;visibility:hidden;"" border=""0"" />",
           @"<a href=""//pluralsight.pxf.io/c/1191850/595237/7490"" id=""595237""><img src=""//a.impactradius-go.com/display-ad/7490-595237"" border=""0"" alt="""" width=""468"" height=""60""/></a><img height=""0"" width=""0"" src=""//pluralsight.pxf.io/i/1191850/595237/7490"" style=""position:absolute;visibility:hidden;"" border=""0"" />",
           @"<a href=""//courses.wilderminds.com/p/vue-js-by-example""><img src=""/img/vue-ad.jpg"" border=""0"" alt="""" width=""468"" height=""60""/></a>",
-          @"<a href=""//courses.wilderminds.com/p/bootstrap-4-by-example""><img src=""/img/bs-ad.jpg"" border=""0"" alt="""" width=""468"" height=""60""/></a>")
+          @"<a href=""//courses.wilderminds.com/p/bootstrap-4-by-example""><img src=""/img/bs-ad.jpg"" border=""0"" alt="""" width=""468"" height=""60""/></a>",
+          @"<a href=""//pluralsight.com/courses/aspnet-core-grpc""><img src=""/img/ads/grpc-468x60.jpg"" border=""0"" alt="""" width=""468"" height=""60""/></a>"
+        )
 
       };
       var now = DateTime.Now;
@@ -53,7 +55,8 @@ namespace WilderBlog.Services
           DateTime.MaxValue.ToString(),
           @"<a href=""//pluralsight.pxf.io/c/1191850/431402/7490"" id=""431402""><img src=""//a.impactradius-go.com/display-ad/7490-431402"" border=""0"" alt="""" width=""250"" height=""250""/></a><img height=""0"" width=""0"" src=""//pluralsight.pxf.io/i/1191850/431402/7490"" style=""position:absolute;visibility:hidden;"" border=""0"" />",
           @"<a href=""//pluralsight.pxf.io/c/1191850/595231/7490"" id=""595231""><img src=""//a.impactradius-go.com/display-ad/7490-595231"" border=""0"" alt="""" width=""250"" height=""250""/></a><img height=""0"" width=""0"" src=""//pluralsight.pxf.io/i/1191850/595231/7490"" style=""position:absolute;visibility:hidden;"" border=""0"" />",
-          @"<a href=""//pluralsight.pxf.io/c/1191850/595246/7490"" id=""595246""><img src=""//a.impactradius-go.com/display-ad/7490-595246"" border=""0"" alt="""" width=""250"" height=""250""/></a><img height=""0"" width=""0"" src=""//pluralsight.pxf.io/i/1191850/595246/7490"" style=""position:absolute;visibility:hidden;"" border=""0"" />"
+          @"<a href=""//pluralsight.pxf.io/c/1191850/595246/7490"" id=""595246""><img src=""//a.impactradius-go.com/display-ad/7490-595246"" border=""0"" alt="""" width=""250"" height=""250""/></a><img height=""0"" width=""0"" src=""//pluralsight.pxf.io/i/1191850/595246/7490"" style=""position:absolute;visibility:hidden;"" border=""0"" />",
+          @"<a href=""//pluralsight.com/courses/aspnet-core-grpc""><img src=""/img/ads/grpc-250x250.jpg"" border=""0"" alt="""" width=""250"" height=""250""/></a>"
         )
 
       };
