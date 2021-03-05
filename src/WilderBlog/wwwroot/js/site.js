df6f69b5ce727d5ff6e9c7df24db28544821534b
Shawn Wildermuth <shawn@wildermuth.com>
2021-03-05 17:25:52 -0500

    Add back top menu

--- src/WilderBlog/wwwroot/js/site.js
+++ src/WilderBlog/wwwroot/js/site.js
@@ -4,11 +4,22 @@
 // Search Form
 document.addEventListener("DOMContentLoaded", function (event) {
 
+  /**********************Menu ************************************************** */
+
+  let menu = document.getElementById("menu");
+  let menuButton = document.getElementById("menu-button");
+  if (menu && menuButton) {
+    menuButton.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    });
+  }
+
+
   /***************** Search **************************************************** */
 
-  var searchForm = document.getElementById("searchForm");
+  let searchForm = document.getElementById("searchForm");
   if (searchForm) {
-    var searchInput = document.getElementById("searchInput");
+    let searchInput = document.getElementById("searchInput");
     searchForm.addEventListener("submit", function (e) {
       window.location = "/search/" + encodeURI(searchInput.value);
       e.preventDefault();
@@ -32,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   /***************** Lazy Images **************************************************** */
 
   // Support Lazy Loading of Images
-  var lazyLoadInstance = new LazyLoad({
+  let lazyLoadInstance = new LazyLoad({
     elements_selector: ".lazy"
     // ... more custom settings?
   });
@@ -105,7 +116,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   /***************** About **************************************************** */
   const openSourceList = document.getElementById("openSourceList");
   if (openSourceList) {
-    var template = _.template(
+    let template = _.template(
 `<div class="border border-gray-100 rounded m-1 p-1">
     <a href='<%= html_url %>' target='blank'><h4><%= name %></h4></a>
     <p class="text-sm"><%= description %></p>
@@ -115,7 +126,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     fetch("https://api.github.com/users/shawnwildermuth/repos?type=owner&sort=pushed")
       .then(result => result.json())
       .then(function (data) {
-        var results = _.filter(data, function (item) {
+        let results = _.filter(data, function (item) {
           return !item.fork && item.watchers_count > 0 && item.description;
         });
         results = _.orderBy(results, ["stargazers_count"], ["desc"]);
